1905214f4848ca6aa316d31c732e4ad61b4e27fe
edpxdk <adetarakapranata123@gmail.com>
2024-06-23 09:24:21 +0800

    update

---
+++ script.js
@@ -0,0 +1,6 @@
+// Toggle Class Active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
